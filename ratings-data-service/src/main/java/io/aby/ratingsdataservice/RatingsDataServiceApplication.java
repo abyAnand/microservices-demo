4c28259fc9c44923679d467e88447d0dbb0af149
abyAnand <abianand382@gmail.com>
2023-04-03 11:30:11 +0530

    Added Application.java files

---
+++ ratings-data-service/src/main/java/io/aby/ratingsdataservice/RatingsDataServiceApplication.java
@@ -0,0 +1,13 @@
+package io.aby.ratingsdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsDataServiceApplication.class, args);
+	}
+
+}
